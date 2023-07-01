df99f2206605e58cc3d3f48ad9494ea3d9c2c92b
Amanmandal-M <amanmandal644@gmail.com>
2023-07-01 13:31:03 +0530

    Updated code with latest functionalities

--- index.py
+++ index.py
@@ -1,44 +1,88 @@
-def displayInventories(itemsInventory):
+def display_inventory(items_inventory):
     print("\nSnack Inventory:")
-    for allSnacks, quantityOfSnack in itemsInventory.items():
-        print(f"{allSnacks}: {quantityOfSnack}")
+    for snack, quantity in items_inventory.items():
+        print(f"{snack}: {quantity}")
 
 
-def record_sale(sales, snack):
-    if snack in sales:
-        sales[snack] += 1
+def add_snack(inventory):
+    snack = input("\nEnter the snack name: ")
+    quantity = input("Enter the quantity: ")
+    try:
+        quantity = int(quantity)
+        inventory[snack] = quantity
+        print(f"{snack} added successfully!")
+    except ValueError:
+        print("Invalid quantity. Please enter a valid integer.")
+
+
+def update_snack(inventory):
+    snack = input("\nEnter the snack name to update: ")
+    if snack in inventory:
+        quantity = input("Enter the new quantity: ")
+        try:
+            quantity = int(quantity)
+            inventory[snack] = quantity
+            print(f"{snack} quantity updated successfully!")
+        except ValueError:
+            print("Invalid quantity. Please enter a valid integer.")
     else:
-        sales[snack] = 1
+        print("Snack not found in the inventory.")
 
 
-def mainSection():
-    inventoryLists = {"Samosa": 10, "Biscuits": 15, "Dhosa": 5}
-    saleData = {}
+def delete_snack(inventory):
+    snack = input("\nEnter the snack name to delete: ")
+    if snack in inventory:
+        del inventory[snack]
+        print(f"{snack} deleted successfully!")
+    else:
+        print("Snack not found in the inventory.")
+
+
+def record_sale(sales, snack, inventory):
+    if snack in inventory:
+        if inventory[snack] > 0:
+            sales[snack] = sales.get(snack, 0) + 1
+            inventory[snack] -= 1
+            print("Sale recorded successfully!")
+        else:
+            print("Snack is not available.")
+    else:
+        print("Snack not found in the inventory.")
+
+
+def main_section():
+    inventory_lists = {"Samosa": 10, "Biscuits": 15, "Dhosa": 5}
+    sale_data = {}
 
     while True:
         print("\nMenu:")
         print("1. Display Inventory")
-        print("2. Sale Record")
-        print("3. Exit")
+        print("2. Add Snack")
+        print("3. Update Snack Quantity")
+        print("4. Delete Snack")
+        print("5. Sale Record")
+        print("6. Exit")
 
-        choices = input("Enter your choice (1-3): ")
+        choice = input("Enter your choice (1-6): ")
 
-        if choices == "1":
-            displayInventories(inventoryLists)
-        elif choices == "2":
-            snack = input("Enter the snack name: ")
-            if snack in inventoryLists:
-                record_sale(saleData, snack)
-                inventoryLists[snack] -= 1
-                print("Sale recorded successfully!")
-            else:
-                print("Snack not available in the inventory.")
-        elif choices == "3":
+        if choice == "1":
+            display_inventory(inventory_lists)
+        elif choice == "2":
+            add_snack(inventory_lists)
+        elif choice == "3":
+            update_snack(inventory_lists)
+        elif choice == "4":
+            delete_snack(inventory_lists)
+        elif choice == "5":
+            snack = input("\nEnter the snack name: ")
+            record_sale(sale_data, snack, inventory_lists)
+        elif choice == "6":
             break
         else:
             print("Invalid choice. Please try again.")
+
     print("Exiting the program.")
 
 
 if __name__ == "__main__":
-    mainSection()
+    main_section()
